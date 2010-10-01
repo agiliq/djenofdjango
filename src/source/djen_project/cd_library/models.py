661a33034e8e252930f1bc060b7ddbf261b7b1cd
Javed Khan <javed@agiliq.com>
2010-10-01 15:32:54 +0530

    more models explanation

--- src/source/djen_project/cd_library/models.py
+++ src/source/djen_project/cd_library/models.py
@@ -12,10 +12,11 @@ GENRE_CHOICES = (
 
 class CD(models.Model):
     title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
     artist = models.CharField(max_length=40)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     genre = models.CharField(max_length=1, choices=GENRE_CHOICES)
 
     def __unicode__(self):
-        return "%s by %s, %Y" %(self.title, self.artist, self.date.year)
+        return "%s by %s, %s" %(self.title, self.artist, self.date.year)
 
