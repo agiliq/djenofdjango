3aa1e1006948d7f0ac27a36d9a17b1c65294bb04
Javed Khan <javed@agiliq.com>
2010-10-05 18:59:22 +0530

    urlify

--- src/source/djen_project/pastebin/urls.py
+++ src/source/djen_project/pastebin/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from models import Paste
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.create_update.create_object', { 'model': Paste }),
+    url(r'^$', 'django.views.generic.create_update.create_object', { 'model': Paste }, name='pastebin_paste_create'),
     url(r'^paste/(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail', { 'queryset': Paste.objects.all() }, name='pastebin_paste_detail'),
     url(r'^pastes/$', 'django.views.generic.list_detail.object_list', { 'queryset': Paste.objects.all() }, name='pastebin_paste_list'),
 )
