03e2f12355d877e8cf9235c7d199965e765e3184
Javed Khan <javed@agiliq.com>
2010-10-05 18:24:47 +0530

    added view all link

--- src/source/djen_project/pastebin/urls.py
+++ src/source/djen_project/pastebin/urls.py
@@ -5,6 +5,6 @@ from models import Paste
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.create_update.create_object', { 'model': Paste }),
     url(r'^paste/(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail', { 'queryset': Paste.objects.all() }, name='pastebin_paste_detail'),
-    url(r'^pastes/$', 'django.views.generic.list_detail.object_list', { 'queryset': Paste.objects.all() }),
+    url(r'^pastes/$', 'django.views.generic.list_detail.object_list', { 'queryset': Paste.objects.all() }, name='pastebin_paste_list'),
 )
 
