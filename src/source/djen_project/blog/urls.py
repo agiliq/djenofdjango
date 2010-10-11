f61b9f41fe4a59935f81efba7c2f77fe584fcebc
Javed Khan <javed@agiliq.com>
2010-10-11 17:00:15 +0530

    added monthly, weekly archives

--- src/source/djen_project/blog/urls.py
+++ src/source/djen_project/blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
+from models import Post
+
 urlpatterns = patterns('',
     url(r'^post/(?P<slug>[-\w]+)$', 
         'blog.views.view_post',
@@ -7,5 +9,19 @@ urlpatterns = patterns('',
     url(r'^add/post$', 
         'blog.views.add_post', 
         name='blog_add_post'),
+    url(r'^archive/month/(?P<year>\d+)/(?P<month>\w+)$',
+        'django.views.generic.date_based.archive_month',
+        {
+            'queryset': Post.objects.all(), 
+            'date_field': 'created_on',
+        },
+       ),
+    url(r'^archive/week/(?P<year>\d+)/(?P<week>\d+)$',
+        'django.views.generic.date_based.archive_week',
+        {
+            'queryset': Post.objects.all(), 
+            'date_field': 'created_on',
+        },
+       ),
 )
 
