5c51f211810ba6d4d7403674646ffff483098a9d
Javed Khan <javed@agiliq.com>
2010-10-18 19:08:40 +0530

    added edit view

--- src/source/djen_project/wiki/forms.py
+++ src/source/djen_project/wiki/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from models import Article
 
 class ArticleForm(forms.ModelForm):
+    summary = forms.CharField(max_length=100)
+
     class Meta:
         model = Article
         exclude = ['author', 'slug']
--- src/source/djen_project/wiki/views.py
+++ src/source/djen_project/wiki/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.template import RequestContext
 
-from models import Article
+from models import Article, Edit
 from forms import ArticleForm
 
 @login_required
@@ -19,12 +19,17 @@ def add_article(request):
                               { 'form': form },
                               context_instance=RequestContext(request))
 
-
+@login_required
 def edit_article(request, slug):
     article = get_object_or_404(Article, slug=slug)
     form = ArticleForm(request.POST or None, instance=article)
     if form.is_valid():
         article = form.save()
+        edit = Edit()
+        edit.article = article
+        edit.editor = request.user
+        edit.summary = request.POST.get('summary')
+        edit.save()
         return redirect(article)
     return render_to_response('wiki/article_form.html', 
                               { 
