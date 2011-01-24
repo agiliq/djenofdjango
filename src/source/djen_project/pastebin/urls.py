6b3bc062dbd267127bdd7049ec23fb6b7e8092bc
Javed Khan <javed@agiliq.com>
2011-01-24 17:03:08 +0530

    pep stuff

--- src/source/djen_project/pastebin/urls.py
+++ src/source/djen_project/pastebin/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', CreateView.as_view(model=Paste), name='pastebin_paste_create'),
     url(r'^paste/edit/(?P<pk>\d+)$', UpdateView.as_view(model=Paste), name='pastebin_paste_edit'),
     url(r'^paste/delete/(?P<pk>\d+)$', DeleteView.as_view(model=Paste, success_url='/pastebin/pastes'), name='pastebin_paste_delete'),
-    url(r'^paste/(?P<pk>\d+)$', DetailView.as_view(queryset= Paste.objects.all()), name='pastebin_paste_detail'),
-    url(r'^pastes/$', ListView.as_view(queryset= Paste.objects.all()), name='pastebin_paste_list'),
+    url(r'^paste/(?P<pk>\d+)$', DetailView.as_view(queryset=Paste.objects.all()), name='pastebin_paste_detail'),
+    url(r'^pastes/$', ListView.as_view(queryset=Paste.objects.all()), name='pastebin_paste_list'),
 )
 
