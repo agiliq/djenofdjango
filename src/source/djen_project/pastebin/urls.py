611c72951e3b879cc7285bc6967037951f661c55
Javed Khan <javed@agiliq.com>
2011-01-24 16:31:41 +0530

    use ListView and DetailView

--- src/source/djen_project/pastebin/urls.py
+++ src/source/djen_project/pastebin/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from models import Paste
@@ -7,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', CreateView.as_view(model=Paste), name='pastebin_paste_create'),
     url(r'^paste/edit/(?P<pk>\d+)$', UpdateView.as_view(model=Paste), name='pastebin_paste_edit'),
     url(r'^paste/delete/(?P<pk>\d+)$', DeleteView.as_view(model=Paste, success_url='/pastebin/pastes'), name='pastebin_paste_delete'),
-    url(r'^paste/(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail', { 'queryset': Paste.objects.all() }, name='pastebin_paste_detail'),
-    url(r'^pastes/$', 'django.views.generic.list_detail.object_list', { 'queryset': Paste.objects.all() }, name='pastebin_paste_list'),
+    url(r'^paste/(?P<pk>\d+)$', DetailView.as_view(queryset= Paste.objects.all()), name='pastebin_paste_detail'),
+    url(r'^pastes/$', ListView.as_view(queryset= Paste.objects.all()), name='pastebin_paste_list'),
 )
 
