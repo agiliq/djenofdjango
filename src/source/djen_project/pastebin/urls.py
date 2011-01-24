1099550d1eb5444e604e131428715b4eda29b696
Javed Khan <javed@agiliq.com>
2011-01-24 16:10:06 +0530

    use UpdateView

--- src/source/djen_project/pastebin/urls.py
+++ src/source/djen_project/pastebin/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from models import Paste
 
 urlpatterns = patterns('',
     url(r'^$', CreateView.as_view(model=Paste), name='pastebin_paste_create'),
-    url(r'^paste/edit/(?P<object_id>\d+)$', 'django.views.generic.create_update.update_object', { 'model': Paste }, name='pastebin_paste_edit'),
+    url(r'^paste/edit/(?P<pk>\d+)$', UpdateView.as_view(model=Paste), name='pastebin_paste_edit'),
     url(r'^paste/delete/(?P<object_id>\d+)$', 'django.views.generic.create_update.delete_object', { 'model': Paste, 'post_delete_redirect': '/pastebin/pastes' }, name='pastebin_paste_delete'),
     url(r'^paste/(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail', { 'queryset': Paste.objects.all() }, name='pastebin_paste_detail'),
     url(r'^pastes/$', 'django.views.generic.list_detail.object_list', { 'queryset': Paste.objects.all() }, name='pastebin_paste_list'),
