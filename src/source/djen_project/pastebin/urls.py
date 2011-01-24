620fa668ec56f118ac43afadebc1d5faf4f2140b
Javed Khan <javed@agiliq.com>
2011-01-24 16:02:31 +0530

    updated create_object to CreateView

--- src/source/djen_project/pastebin/urls.py
+++ src/source/djen_project/pastebin/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
-#from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView
 
 from models import Paste
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.create_update.create_object', { 'model': Paste }, name='pastebin_paste_create'),
+    url(r'^$', CreateView.as_view(model=Paste), name='pastebin_paste_create'),
     url(r'^paste/edit/(?P<object_id>\d+)$', 'django.views.generic.create_update.update_object', { 'model': Paste }, name='pastebin_paste_edit'),
     url(r'^paste/delete/(?P<object_id>\d+)$', 'django.views.generic.create_update.delete_object', { 'model': Paste, 'post_delete_redirect': '/pastebin/pastes' }, name='pastebin_paste_delete'),
     url(r'^paste/(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail', { 'queryset': Paste.objects.all() }, name='pastebin_paste_detail'),
