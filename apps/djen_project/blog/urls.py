5c4cd4570900b3a0f1b78ecbf0480d56a7ce9648
yashrastogi16 <yash.rastogi916@gmail.com>
2018-04-03 15:02:06 +0530

    final commit for chapter 4. Updated url's added images, removed literalinclude

--- apps/djen_project/blog/urls.py
+++ apps/djen_project/blog/urls.py
@@ -1,11 +1,11 @@
-from django.urls import re_path, include
+from django.urls import path, include
 from .views import view_post, add_post, PostMonthArchiveView, PostWeekArchiveView
 
 
 urlpatterns = [
-    re_path(r'^post/(?P<slug>[-\w]+)$', view_post, name='blog_post_detail'),
-    re_path(r'^add/post$', add_post, name='blog_add_post'),
-    re_path(r'^archive/<int:year>/month/<int:month>$', PostMonthArchiveView.as_view(month_format='%m'), name='blog_archive_month',),
-    re_path(r'^archive/<int:year>/week/<int:week>$', PostWeekArchiveView.as_view(), name='blog_archive_week'),
+    path('post/<str:slug>', view_post, name='blog_post_detail'),
+    path('add/post', add_post, name='blog_add_post'),
+    path('archive/<int:year>/month/<int:month>', PostMonthArchiveView.as_view(month_format='%m'), name='blog_archive_month',),
+    path('archive/<int:year>/week/<int:week>', PostWeekArchiveView.as_view(), name='blog_archive_week'),
         ]
 
