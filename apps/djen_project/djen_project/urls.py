05b8f98906fbdc3150860e35e5b9eaa19fa7a0d1
yashrastogi16 <yash.rastogi916@gmail.com>
2018-04-03 11:42:27 +0530

    final commit chapter 2

--- apps/djen_project/djen_project/urls.py
+++ apps/djen_project/djen_project/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 from django.contrib.auth.views import login
 
 urlpatterns = [
-    path(r'accounts/login/', login),
-    path(r'admin/', admin.site.urls),
-    path(r'pastebin/', include('pastebin.urls')),
-    path(r'blog/', include('blog.urls')),
-    path(r'wiki/', include('wiki.urls')),
+    path('accounts/login/', login),
+    path('admin/', admin.site.urls),
+    path('pastebin/', include('pastebin.urls')),
+    path('blog/', include('blog.urls')),
+    path('wiki/', include('wiki.urls')),
 ]
